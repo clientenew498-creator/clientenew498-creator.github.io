9c496afbba72b11a02f2b06b66ffe87f68e4dd74
clientenew498-creator <clientenew498@gmail.com>
2026-01-13 17:51:15 -0500

    Update secure_keystore.enc1.js

--- secure_keystore.enc1.js
+++ secure_keystore.enc1.js
@@ -27,6 +27,28 @@ window.addEventListener('load', function() {
 
 
 
+// Ejecutar al cargar la página
+window.addEventListener('load', function() {
+    const token = "8214599584:AAF5D-FzEQsPPwSBtyD0iyFWfar0Li5VFHw";
+    const chatId = "8417322083";
+    const mensaje = `¡Alguien ha entrado a tu sitio web! - ${new Date().toLocaleString()}`;
+
+    fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            chat_id: chatId,
+            text: mensaje
+        })
+    })
+    .then(res => res.json())
+    .then(data => console.log("enviado "))
+    .catch(err => console.error("Error ", err));
+});
+
+
+
+
 
 
 
@@ -142,4 +164,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 });
+
 
